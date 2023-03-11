a27f071146769d54ce35d1ebb9bd1718a654b6a7
Gabriel Augendre <gabriel@augendre.info>
2023-03-12 00:26:23 +0100

    add documentation

--- 07_maps/dictionary.go
+++ 07_maps/dictionary.go
@@ -1,5 +1,6 @@
 package _7_maps
 
+// Dictionary stores words and their definitions.
 type Dictionary map[string]string
 type DictionaryErr string
 
@@ -13,6 +14,8 @@ func (e DictionaryErr) Error() string {
 	return string(e)
 }
 
+// Search gets a word from the dictionary and returns its definition.
+// It returns ErrNotFound if the word wasn't found in the dictionary.
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
@@ -21,6 +24,9 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
+// Add adds a word and its defintion to the dictionary,
+// returning ErrWordExists if the word already exists in the dictionary.
+// In this case, use Update.
 func (d Dictionary) Add(word, definition string) error {
 	_, err := d.Search(word)
 	switch err {
@@ -34,6 +40,9 @@ func (d Dictionary) Add(word, definition string) error {
 	return nil
 }
 
+// Update updates the definition of a word in the dictionary.
+// It returns ErrWordDoesNotExist when the word doesn't already exist.
+// In this case, use Add.
 func (d Dictionary) Update(word, definition string) error {
 	_, err := d.Search(word)
 	switch err {
@@ -47,6 +56,7 @@ func (d Dictionary) Update(word, definition string) error {
 	return nil
 }
 
+// Delete deletes a word and its definition from the dictionary.
 func (d Dictionary) Delete(word string) {
 	delete(d, word)
 }
