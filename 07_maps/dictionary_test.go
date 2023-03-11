8cff7bba471bbe37bf28c015284cea7b1e54050c
Gabriel Augendre <gabriel@augendre.info>
2023-03-11 13:32:05 +0100

    Implement dict add

--- 07_maps/dictionary_test.go
+++ 07_maps/dictionary_test.go
@@ -2,15 +2,65 @@ package _7_maps
 
 import "testing"
 
+var word = "test"
+var definition = "this is just a test"
+
 func TestSearch(t *testing.T) {
-	word := "test"
-	definition := "this is just a test"
-	dictionary := map[string]string{word: definition}
+	dictionary := Dictionary{word: definition}
 
-	got := Search(dictionary, word)
-	want := definition
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search(word)
+		want := definition
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := ErrNotFound
+
+		assertError(t, err, want)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dict := Dictionary{}
+		err := dict.Add(word, definition)
+		assertNoError(t, err)
+
+		got, err := dict.Search(word)
+		want := definition
+		assertNoError(t, err)
+		assertStrings(t, got, want)
+	})
+	t.Run("existing word", func(t *testing.T) {
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new def")
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func assertDefinition(tb testing.TB, dictionary Dictionary, word, want string) {
+	tb.Helper()
+	got, err := dictionary.Search(word)
+	assertNoError(tb, err)
+	if got != want {
+		tb.Errorf("got %q want %q given %q", got, want, word)
+	}
+}
+
+func assertError(tb testing.TB, got, want error) {
+	tb.Helper()
+	if got != want {
+		tb.Errorf("got error %q, want %q", got, want)
+	}
+}
+
+func assertNoError(tb testing.TB, got error) {
+	tb.Helper()
+	assertError(tb, got, nil)
 }
 
 func assertStrings(t testing.TB, got string, want string) {
