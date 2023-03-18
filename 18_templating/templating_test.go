0d555b69779c8c64d612f0b6e69d75ff48fad949
Gabriel Augendre <gabriel@augendre.info>
2023-03-18 19:52:07 +0100

    Improve perf

--- 18_templating/templating_test.go
+++ 18_templating/templating_test.go
@@ -3,6 +3,7 @@ package templating_test
 import (
 	"bytes"
 	"github.com/approvals/go-approval-tests"
+	"io"
 	templating "learn-go-with-tests/18_templating"
 	"testing"
 )
@@ -15,13 +16,36 @@ func TestRender(t *testing.T) {
 		Tags:        []string{"go", "tdd"},
 	}
 
+	renderer, err := templating.NewPostRenderer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	t.Run("convert a single post to HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
 
-		if err := templating.Render(&buf, post); err != nil {
+		if err = renderer.Render(&buf, post); err != nil {
 			t.Fatal(err)
 		}
 
 		approvals.VerifyString(t, buf.String())
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	post := templating.Post{
+		Title:       "Hello, world",
+		Body:        "This is a post",
+		Description: "This is a description",
+		Tags:        []string{"go", "tdd"},
+	}
+	renderer, err := templating.NewPostRenderer()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		renderer.Render(io.Discard, post)
+	}
 }
