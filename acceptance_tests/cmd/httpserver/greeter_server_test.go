a0f7ec10cd9d2f6b1561b13310593dc168eb7fa8
Gabriel Augendre <gabriel@augendre.info>
2023-03-19 18:04:57 +0100

    Extract docker setup

--- acceptance_tests/cmd/httpserver/greeter_server_test.go
+++ acceptance_tests/cmd/httpserver/greeter_server_test.go
@@ -1,41 +1,24 @@
 package main_test
 
 import (
-	"context"
+	"fmt"
+	"github.com/Crocmagnon/learn-go-with-tests/acceptance_tests/adapters"
 	"github.com/Crocmagnon/learn-go-with-tests/acceptance_tests/adapters/httpserver"
 	"github.com/Crocmagnon/learn-go-with-tests/acceptance_tests/specifications"
-	"github.com/alecthomas/assert/v2"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
 	"net/http"
 	"testing"
 	"time"
 )
 
 func TestGreeterServer(t *testing.T) {
-	ctx := context.Background()
-	req := testcontainers.ContainerRequest{
-		FromDockerfile: testcontainers.FromDockerfile{
-			Context:       "../../../.",
-			Dockerfile:    "./acceptance_tests/Dockerfile",
-			PrintBuildLog: true,
-		},
-		ExposedPorts: []string{"8080:8080"},
-		WaitingFor:   wait.ForHTTP("/").WithPort("8080"),
-	}
-	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	})
-	assert.NoError(t, err)
-	t.Cleanup(func() {
-		assert.NoError(t, container.Terminate(ctx))
-	})
+	var (
+		port           = "8080"
+		dockerfilePath = "./acceptance_tests/Dockerfile"
+		baseURL        = fmt.Sprintf("http://127.0.0.1:%s", port)
+		client         = http.Client{Timeout: 1 * time.Second}
+		driver         = httpserver.Driver{BaseURL: baseURL, Client: &client}
+	)
 
-	client := http.Client{
-		Timeout: 1 * time.Second,
-	}
-
-	driver := httpserver.Driver{BaseURL: "http://127.0.0.1:8080", Client: &client}
+	adapters.StartDockerServer(t, dockerfilePath, port)
 	specifications.GreetSpecification(t, driver)
 }
