d874a8af17e2c920f7de6e19f0874256651c0a29
Gabriel Augendre <gabriel@augendre.info>
2023-03-19 22:53:47 +0100

    Implement new requirement for greet

--- acceptance_tests/domain/interactions/greet.go
+++ acceptance_tests/domain/interactions/greet.go
@@ -3,6 +3,9 @@ package interactions
 import "fmt"
 
 func Greet(name string) string {
+	if name == "" {
+		name = "world"
+	}
 	return fmt.Sprintf("Hello, %s", name)
 }
 
--- acceptance_tests/domain/interactions/greet_test.go
+++ acceptance_tests/domain/interactions/greet_test.go
@@ -3,14 +3,23 @@ package interactions_test
 import (
 	go_specs_greet "github.com/Crocmagnon/learn-go-with-tests/acceptance_tests/domain/interactions"
 	"github.com/Crocmagnon/learn-go-with-tests/acceptance_tests/specifications"
+	"github.com/alecthomas/assert/v2"
 	"testing"
 )
 
 func TestGreet(t *testing.T) {
-	specifications.GreetSpecification(
-		t,
-		specifications.GreetAdapter(go_specs_greet.Greet),
-	)
+	t.Run("specification", func(t *testing.T) {
+		specifications.GreetSpecification(
+			t,
+			specifications.GreetAdapter(go_specs_greet.Greet),
+		)
+	})
+	t.Run("default to world", func(t *testing.T) {
+		got := go_specs_greet.Greet("")
+		want := "Hello, world"
+
+		assert.Equal(t, got, want)
+	})
 }
 
 func TestCurse(t *testing.T) {
